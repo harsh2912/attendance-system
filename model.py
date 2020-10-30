cd8adbee55ce8bce1c90a7072a9ff0fc467541e8
harsh2912 <harshshrm94@gmail.com>
2020-10-30 20:15:44 +0000

    model file

---
+++ model.py
@@ -0,0 +1,21 @@
+import  sys
+sys.path.append('deploy/')
+sys.path.append('RetinaFace')
+from face_model_2 import FaceModel
+
+
+class args:
+    def __init__(self):
+        self.image_size = '112,112'
+        self.model = 'models/insightface/model-r100-ii/model,0'
+        self.ga_model = ''
+        self.gpu = 0
+        self.det = 0
+        self.flip =False
+        self.threshold = 1.24
+        self.detector_path = 'models/retinaface/R50'
+        self.det_threshold = 0.7
+        
+
+Args = args()
+Model  =  FaceModel(Args)
